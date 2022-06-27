5d7dd631f15e6f02216d5e76071602000f28309b
Mariya Zhuchina <zhuchina@yandex.ru>
2022-06-27 22:50:17 +0300

    The 1st version

---
+++ src/utils.py
@@ -0,0 +1,42 @@
+import logging
+
+from requests import RequestException
+
+from exceptions import ParserFindTagException
+
+
+def get_response(session, url):
+    try:
+        response = session.get(url)
+        response.encoding = 'utf-8'
+        return response
+    except RequestException:
+        logging.exception(
+            f'Возникла ошибка при загрузке страницы {url}',
+            stack_info=True
+        )
+
+
+def find_tag(soup, tag, attrs=None):
+    searched_tag = soup.find(tag, attrs=(attrs or {}))
+    if searched_tag is None:
+        error_msg = f'Не найден тег {tag} {attrs}'
+        logging.error(error_msg, stack_info=True)
+        raise ParserFindTagException(error_msg)
+    return searched_tag
+
+
+def find_sibling(soup, search='next'):
+    searched_tag = (
+        soup.find_next_sibling(),
+        soup.find_previous_sibling()
+    )[search == 'prev']
+    # if search == 'prev':
+    #     searched_tag = soup.find_previous_sibling()
+    # else:
+    #     searched_tag = soup.find_next_sibling()
+    if searched_tag is None:
+        error_msg = 'Не найден соседний одноуровневый тег.'
+        logging.error(error_msg, stack_info=True)
+        raise ParserFindTagException(error_msg)
+    return searched_tag
